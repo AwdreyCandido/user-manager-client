69019a22e7b4359ddd27a105e248cd970632f6b6
AwdreyCandido <awdrey.dev@gmail.com>
2024-07-25 02:27:59 -0300

    initial scaffolding config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h1" component="h2">
+        Hello World!
+      </Typography>
+    </div>
+  );
+}
+
+export default App;
